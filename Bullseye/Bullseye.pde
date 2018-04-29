3ea48bde970ef241c3eb4d6e41dbff68f6abb78d
Rohan890 <league@iMac-2.attlocal.net>
2018-04-29 15:56:39 -0700

    Bullseye/obedient robot

---
+++ Bullseye/Bullseye.pde
@@ -0,0 +1,15 @@
+void draw(){
+ellipse(100,100,100,100);
+ellipse(100,100,90,90);
+ellipse(100,100,80,80);
+ellipse(100,100,70,70);
+ellipse(100,100,60,60);
+ellipse(100,100,50,50);
+ellipse(100,100,40,40);
+ellipse(100,100,30,30);
+ellipse(100,100,20,20);
+ellipse(100,100,10,10);
+}
+void setup(){
+size(800,600);
+}
--- src/elseif/obedientrobot.java
+++ src/elseif/obedientrobot.java
@@ -1,7 +1,37 @@
 package elseif;
 
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class obedientrobot {
-public static void main(String[] args) {
-	
-}
+	static Robot obedientRobot = new Robot();
+
+	public static void main(String[] args) {
+		String shape = JOptionPane.showInputDialog("What shape to you want a triangle or a square");
+		if (shape.equals("square")) {
+			drawSquare();
+		}
+		else {
+			drawTriangle();
+		}
+	}
+
+	static void drawSquare() {
+		obedientRobot.hide();
+		obedientRobot.sparkle();
+		for (int i = 0; i < 5; i++) {
+			obedientRobot.penDown();
+			obedientRobot.move(90);
+			obedientRobot.turn(90);
+		}
+	}
+
+	static void drawTriangle() {
+		for (int i = 0; i < 4; i++) {
+			obedientRobot.penDown();
+			obedientRobot.turn(120);
+			obedientRobot.move(90);
+		}
+	}
 }
